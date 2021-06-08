bb7aea9fab042ebc47c84a9ff754dc92dfda0c53
ryonasso <ryounaoyuki@gmail.com>
2021-06-08 15:55:45 +0900

    feat : chakra-uiの導入

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import React, { useState } from "react";
+import { Heading } from "@chakra-ui/react";
 
 export default function Home() {
   const [hour, setHour] = useState("25:00");
   return (
     <div>
-      <p>Pomodoro Timer</p>
+      <Heading>Pomodoro Timer</Heading>
       <p>{hour}</p>
       <button onClick={() => setHour("30:00")}>Start</button>
     </div>
