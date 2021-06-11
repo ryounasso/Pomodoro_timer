4512ff763ead1878592b5b6e6629f0461ebe03a0
ryonasso <ryounaoyuki@gmail.com>
2021-06-11 23:03:44 +0900

    fix : カウントダウン方法の変更

--- components/Timer.jsx
+++ components/Timer.jsx
@@ -1,37 +1,60 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Button } from "@chakra-ui/react";
 
 export function Timer() {
   const [hour, setHour] = useState({ min: 30, sec: 0 });
+  const [prevTime, setPrevTime] = useState(hour.min * 60 + hour.sec);
   const [isRun, setIsRun] = useState(false);
-
-  let restTime = hour.min * 60 + hour.sec;
+  const [id, setId] = useState();
 
   const countDown = () => {
+    const restTime = hour.min * 60 + hour.sec;
     let newRestTime = restTime - 1;
+    setPrevTime(prevTime - 1);
+    console.log("restTime", prevTime);
     return newRestTime;
   };
 
   const toTime = () => {
-    let time = countDown();
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+    const time = countDown();
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
     setHour({ min: minutes, sec: seconds });
+    console.log("time", time);
+    console.log(hour);
+  };
+
+  const start = () => {
+    if (!isRun) {
+      setId(setInterval(toTime, 1000));
+      setIsRun(true);
+    }
+    console.log(isRun);
+  };
+
+  const stop = () => {
+    if (isRun) {
+      clearInterval(id);
+      setIsRun(false);
+    }
+    console.log("stop", prevTime);
   };
 
   useEffect(() => {
-    const id = setInterval(() => {
-      toTime();
-    }, 1000);
-    return () => {
+    if (!isRun) {
+      setId(setInterval(toTime, 1000));
+    } else {
       clearInterval(id);
-    };
-  }, [hour]);
+    }
+    console.log(isRun);
+    return () => clearInterval(id);
+  }, [isRun]);
 
   return (
     <div>
       {hour.min} : {hour.sec}
-      <Button onClick={() => setIsRun(!isRun)}>Time</Button>
+      <Button onClick={() => setIsRun(true)}>Start</Button>
+      <Button onClick={() => setIsRun(false)}>Stop</Button>
     </div>
   );
 }
