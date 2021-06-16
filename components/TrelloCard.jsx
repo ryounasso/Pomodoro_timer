89a9b3fdaad0ce7441481c3b78c0c0dffcd8c37c
ryonasso <ryounaoyuki@gmail.com>
2021-06-16 17:01:49 +0900

    feat : useSWRの追加

--- components/TrelloCard.jsx
+++ components/TrelloCard.jsx
@@ -1,3 +1,11 @@
+import useSWR from "swr";
+
+const fetchar = () => fetch("/api/user");
+
 export function TrelloCard() {
+  const { data, error } = useSWR("/api/user", fetchar);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
   return <div>trelloだよ</div>;
 }
