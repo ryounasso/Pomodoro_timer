2fb55a045e8e535e88bb3daeed9739a2abb111df
ryonasso <ryounaoyuki@gmail.com>
2021-06-17 23:03:26 +0900

    feat : trelloのAPIを叩けるようにした

--- components/TrelloCard.jsx
+++ components/TrelloCard.jsx
@@ -1,9 +1,9 @@
 import useSWR from "swr";
 
-const fetchar = () => fetch("/api/user");
+const fetchar = () => fetch("https://trello.com/1/members/aiiver/boards?key="+process.env.NEXT_PUBLIC_KEY+"&token="+process.env.NEXT_PUBLIC_TOKEN+"&fields=name");
 
 export function TrelloCard() {
-  const { data, error } = useSWR("/api/user", fetchar);
+  const { data, error } = useSWR("https://trello.com/1/members/aiiver/boards?key="+process.env.NEXT_PUBLIC_KEY+"&token="+process.env.NEXT_PUBLIC_TOKEN+"&fields=name", fetchar);
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
