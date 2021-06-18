637a4fae7022760f6d8a1b560f7a16861b4c165d
ryonasso <ryounaoyuki@gmail.com>
2021-06-18 22:47:35 +0900

    feat : trelloのカードの情報を取得

--- components/TrelloCard.jsx
+++ components/TrelloCard.jsx
@@ -1,11 +1,56 @@
+import React from "react";
 import useSWR from "swr";
+import Loader from "react-loader-spinner";
+import { Center } from "@chakra-ui/react";
+import axios from "axios";
+import { Card } from "./Card";
 
-const fetchar = () => fetch("https://trello.com/1/members/aiiver/boards?key="+process.env.NEXT_PUBLIC_KEY+"&token="+process.env.NEXT_PUBLIC_TOKEN+"&fields=name");
+const fetchar = () =>
+  axios
+    .get(
+      "https://api.trello.com/1/lists/" +
+        process.env.NEXT_PUBLIC_LIST_TOMORROW +
+        "/cards?key=" +
+        process.env.NEXT_PUBLIC_KEY +
+        "&token=" +
+        // process.env.NEXT_PUBLIC_TOKEN +
+        process.env.NEXT_PUBLIC_TOKEN
+      // "&fields=name"
+    )
+    .then((res) => res.data);
 
 export function TrelloCard() {
-  const { data, error } = useSWR("https://trello.com/1/members/aiiver/boards?key="+process.env.NEXT_PUBLIC_KEY+"&token="+process.env.NEXT_PUBLIC_TOKEN+"&fields=name", fetchar);
+  const { data, error } = useSWR(
+    "https://api.trello.com/1/lists/" +
+      process.env.NEXT_PUBLIC_LIST_TOMORROW +
+      "/cards?key=" +
+      process.env.NEXT_PUBLIC_KEY +
+      "&token=" +
+      // process.env.NEXT_PUBLIC_TOKEN +
+      process.env.NEXT_PUBLIC_TOKEN,
+    // "&fields=name",
+
+    fetchar
+  );
+
+  console.log(data);
 
   if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
-  return <div>trelloだよ</div>;
+  if (!data)
+    return (
+      <Center>
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+        />
+      </Center>
+    );
+  return (
+    <div>
+      <Card name={data[0].name} />
+    </div>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
