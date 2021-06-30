0b47ab65af6d297a47bcf86359633e7548e7fe5e
ryonasso <ryounaoyuki@gmail.com>
2021-06-30 22:58:24 +0900

    fix : gapiをscriptタグで読み込み

--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -1,65 +1,128 @@
 import React, { useEffect } from "react";
-// import ScriptTag from "react-script-tag";
 
 export function Calendar() {
-  let googleAuth;
-
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://apis.google.com/js/api.js";
     script.async = true;
     script.onload = "this.onload=function(){};handleClientLoad()";
+    script.onreadystatechange =
+      "if (this.readyState === 'complete') this.onload()";
     document.body.appendChild(script);
   }, []);
 
-  function initClient() {
+  function handleClientLoad() {
     gapi.load("client:auth2", initClient);
+  }
+
+  let GoogleAuth;
+
+  function initClient() {
     gapi.client
       .init({
         apiKey: process.env.NEXT_PUBLIC_GOOGLE_APIKEY,
         clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-        scope: "https://www.googleapis.com/auth/calendar.readonly",
         discoveryDocs: [
           "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
         ],
+        scope: "https://www.googleapis.com/auth/calendar.readonly",
       })
-      .then(function () {
-        googleAuth = gapi.auth2.getAuthInstance();
+      .then(
+        function () {
+          // GoogleAuth = gapi.auth2.getAuthInstance();
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
 
-        // Listen for sign-in state changes.
-        GoogleAuth.isSignedIn.listen(updateSigninStatus);
+          // Listen for sign-in state changes.
+          // GoogleAuth.isSignedIn.listen(updateSigninStatus);
+          console.log("success");
+        },
+        function (error) {
+          appendPre(JSON.stringify(error, null, 2));
+        }
+      );
+  }
 
-        console.log("success");
+  let isAuthorized;
+  let currentApiRequest;
+
+  function sendAuthorizedApiRequest(requestDetails) {
+    currentApiRequest = requestDetails;
+    if (isAuthorized) {
+      // Make API request
+      // gapi.client.request(requestDetails)
+
+      // Reset currentApiRequest variable.
+      currentApiRequest = {};
+    } else {
+      GoogleAuth.signIn();
+    }
+  }
+
+  function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+      isAuthorized = true;
+      if (currentApiRequest) {
+        sendAuthorizedApiRequest(currentApiRequest);
+      }
+      listUpcomingEvents();
+      console.log("true");
+    } else {
+      isAuthorized = false;
+      console.log("false");
+    }
+  }
+
+  function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+  }
+
+  function handleSignoutClick() {
+    console.log("ろぐあーうと", gapi);
+    gapi.auth2.getAuthInstance().signOut();
+  }
+
+  function appendPre(message) {
+    const pre = document.getElementById("content");
+    const textContent = document.createTextNode(message + "\n");
+    pre.appendChild(textContent);
+  }
+
+  function listUpcomingEvents() {
+    console.log("イベントとってくるでー");
+    gapi.client.calendar.events
+      .list({
+        calendarId: process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_ID,
+        // calendarId: "primary",
+        timeMin: new Date().toISOString(),
+        showDeleted: false,
+        singleEvents: true,
+        maxResults: 10,
+        orderBy: "startTime",
+      })
+      .then(function (response) {
+        console.log(response);
+        const events = response.result.items;
+        appendPre("Upcoming events:");
+
+        if (events.length > 0) {
+          for (let i = 0; i < events.length; i++) {
+            const event = events[i];
+            let when = event.start.dateTime;
+            if (!when) {
+              when = event.start.date;
+            }
+            appendPre(event.summary + " (" + when + ")");
+          }
+        } else {
+          appendPre("No upcoming events found.");
+        }
       });
   }
 
-  const getEvents = async () => {
-    return new Promise(async (resolve, reject) => {
-      if (ApiCalendar.sign) {
-        ApiCalendar.listEvents({
-          timeMin: new Date().toISOString(),
-          timeMax: new Date().addDays(10).toISOString(),
-          showDeleted: true,
-          maxResults: 10,
-          orderBy: "updated",
-        }).then(({ result }) => {
-          if (result.items) {
-            console.log(result.items);
-          } else {
-            console.log("No Events");
-          }
-          resolve(result);
-        });
-      } else {
-        ApiCalendar.handleAuthClick();
-        resolve(null);
-      }
-    });
-  };
-
   return (
     <div>
-      <button onClick={() => initClient()}>Get</button>
+      <button onClick={() => handleClientLoad()}>Get</button>
     </div>
   );
 }
